e3d05a91b2269b352192df3b2e84e1ef8bb39a4d
Ph4ra0h <xsousamaiax@hotmail.com>
2018-09-26 10:13:23 -0300

    0.0.5

--- App.js
+++ App.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
-import { Text, View, TextInput, Input, Button, Image, TouchableOpacity } from 'react-native';
+import { Text, View, TextInput, Input, Button, Image, TouchableOpacity, AppRegistry, StyleSheet } from 'react-native';
 
 export default class Login extends Component {
   render() {
     return (
 
       <View style={{marginTop: 120}}>
-      <TextInput
-          style={{height: 40, borderWidth: 1,margin: 10, textAlign: 'center', borderRadius: 20}}
-          placeholder="Email"
-        />
 
-      <TextInput
-          style={{height: 40, borderWidth: 1, margin: 10, textAlign: 'center', borderRadius: 20}}
-          placeholder="******"
-        />
 
-        <TouchableOpacity
-        style={{width: 100 ,height: 35, margin: 10, alignSelf: 'center', borderRadius: 20,backgroundColor: '#48BBEC'}}>
-        <Text style={{color: '#fff', textAlign: 'center'}}>Login</Text>
+      <TextInput style={estilo.caixa}
+        placeholder="Email"/>
+
+        <TextInput secureTextEntry={true} style={estilo.caixa}
+          placeholder="*********"/>
+
+        <TouchableOpacity style={estilo.but}>
+        <Text style={estilo.texto}>Login</Text>
         </TouchableOpacity>
+
+        <Image
+        style={{width: 320, height: 480}}
+        source={require('./paper.jpg')}
+      />
 
       </View>
 
     );
   }
 }
+
+const estilo = StyleSheet.create({
+
+  but: {
+    width: 100, height: 35, margin: 10, alignSelf: 'center', borderRadius: 20,backgroundColor: '#48BBEC', justifyContent: 'center'
+  },
+  texto: {
+    color: '#fff', textAlign: 'center'
+  },
+  caixa: {
+    height: 40, borderWidth: 1,margin: 10, textAlign: 'center', borderRadius: 20
+  }
+
+});
+
+AppRegistry.registerComponent('Login', () => Login);
